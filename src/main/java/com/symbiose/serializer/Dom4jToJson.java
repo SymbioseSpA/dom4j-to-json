480fa0f9a7c669a9f497b372b6afc6a37943b09e
Francisco Collao Garate <francisco.collao@symbiose.cl>
2015-06-23 22:46:56 -0300

    ADD: initial commit
    Signed-off-by: Francisco Collao Garate <francisco.collao@symbiose.cl>

---
+++ src/main/java/com/symbiose/serializer/Dom4jToJson.java
@@ -0,0 +1,10 @@
+package com.symbiose.serializer;
+
+
+/**
+ * @author pcollaog
+ *
+ */
+public class Dom4jToJson {
+
+}
---
+++ src/test/java/com/symbiose/serializer/Dom4jToJsonTest.java
@@ -0,0 +1,18 @@
+package com.symbiose.serializer;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author pcollaog
+ *
+ */
+public class Dom4jToJsonTest {
+
+	@Test
+	public final void test() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
