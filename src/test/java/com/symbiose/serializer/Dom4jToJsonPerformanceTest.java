71df7cddd22e69c135a10000e9961159aa574cbf
Francisco Collao Garate <francisco.collao@symbiose.cl>
2015-07-31 11:14:07 -0300

    FIX: Issue #1
    Signed-off-by: Francisco Collao Garate <francisco.collao@symbiose.cl>

--- src/test/java/com/symbiose/serializer/Dom4jToJsonPerformanceTest.java
+++ src/test/java/com/symbiose/serializer/Dom4jToJsonPerformanceTest.java
@@ -11,18 +11,39 @@ import org.junit.Test;
  */
 public class Dom4jToJsonPerformanceTest extends Dom4jToJsonTest {
 
+	private static final int WARM_UP = 5 * 1000;
+
+	private static final int DURATION = 10 * 1000;
+
+	private static final int THREADS = 60;
+
 	@Rule
 	public ContiPerfRule rule = ContiPerfRule.createDefaultRule();
 
+	/**
+	 * 
+	 */
 	public Dom4jToJsonPerformanceTest() {
 		super(false);
 	}
 
+	/**
+	 * @Required(average = 50, max = 500)
+	 */
 	@Test
 	@Override
-	@PerfTest(threads = 60, duration = 10 * 1000)
-	public void shouldSerializeXmlToJson() throws Exception {
-		super.shouldSerializeXmlToJson();
+	@PerfTest(threads = THREADS, duration = DURATION, warmUp = WARM_UP)
+	public void shouldSerialize_case1() throws Exception {
+		super.shouldSerialize_case1();
 	}
 
+	/**
+	 * @Required(average = 700)
+	 */
+	@Test
+	@Override
+	@PerfTest(threads = THREADS, duration = DURATION, warmUp = WARM_UP)
+	public void shouldSerialize_case3() throws Exception {
+		super.shouldSerialize_case3();
+	}
 }
